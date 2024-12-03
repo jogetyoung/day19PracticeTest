1da2eb460377adbea04908b6c5f834730fcec489
jogetyoung <youngjoeljj@gmail.com>
2024-12-03 14:02:32 +0800

    first commit

---
+++ src/test/java/vttp_ssf/Practice/Test/PracticeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp_ssf.Practice.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
